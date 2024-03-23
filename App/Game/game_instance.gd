6e1e81159dbfad0f09fa18cd5e1cbebfcc905136
Opinder <opinder.13@gmail.com>
2024-03-23 13:03:32 +0000

    Update to use the latest Universe api

--- App/Game/game_instance.gd
+++ App/Game/game_instance.gd
@@ -1,27 +1,24 @@
 extends Node
 
-var world: UniverseSimulation
+var universe : Universe
+var simulation: UniverseSimulation
 
 @export var mesh: Mesh
 
-var scenario: RID
-	
 func _enter_tree() -> void:
-	world = UniverseSimulation.new()
-	
-	scenario = $Layers/GameLayer.world_3d.scenario
-	
-	for i in range(100):
-		var id = world.create_instance(mesh.get_rid(), scenario)
-		
-		world.set_instance_pos(id, Vector3(randf_range(-100, 100), randf_range(-100, 100), randf_range(-100, 100)))
+	universe = Universe.new()
 	
 func _exit_tree() -> void:
-	world.stop_galaxy()
+	if simulation != null:
+		simulation.stop_simulation()
+		simulation = null
+		
+	universe = null
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	world.progress(delta)
+	if (!simulation.progress(delta)):
+		printerr("world progress failed")
 	
 	$Layers/GameLayer/Camera.do_camera_controls(delta)
 	
@@ -30,10 +27,17 @@ func _process(delta: float) -> void:
 	$Layers/StarsLayer/Camera.transform.basis = transform.basis
 	$Layers/StarsLayer/Stars.position = (-transform.origin / 10000)
 	$Layers/StarsLayer/Galaxies.position = ((-transform.origin / 100000) / 100000)
-
+	
 	$Layers/GameLayer/Speed.text = "Speed: " + str($Layers/GameLayer/Camera.accelerator)
 
 func load_galaxy(galaxy_directory: String) -> void:
-	world.set_threads(12)
-	world.start_rest_server()
-	world.start_local_galaxy(galaxy_directory)
+	simulation = universe.start_local_galaxy(galaxy_directory)
+	
+	simulation.start_simulation()
+	
+	var scenario: RID = $Layers/GameLayer.world_3d.scenario
+	
+	for i in range(10):
+		var id: int = simulation.create_instance(mesh.get_rid(), scenario)
+		
+		simulation.set_instance_pos(id, Vector3(randf_range(-100, 100), randf_range(-100, 100), randf_range(-100, 100)))
