e9ff07f4dd8d3c622426ee6bccc647c36247a8c4
Opinder <opinder.13@gmail.com>
2025-03-06 23:14:04 +0000

    Make UniverseSimulation a singleton

--- App/Game/game.gd
+++ App/Game/game.gd
@@ -1,26 +1,18 @@
 extends Screen
 
-var simulation: UniverseSimulation
-
 @onready var instance_schematic = preload("res://App/Game/game_instance.tscn")
-
+	
 func _enter_tree() -> void:
-	simulation = UniverseSimulation.new()
+	UniverseSimulation.set_render_context(get_viewport().find_world_3d().scenario)
 	
-	simulation.initialize(ProjectSettings.get_setting("voxel_game/universe/path"), get_viewport().find_world_3d().scenario)
-	
-	simulation.start_simulation(UniverseSimulation.THREAD_MODE_MULTI_THREADED)
+	UniverseSimulation.start_simulation(UniverseSimulation.THREAD_MODE_MULTI_THREADED)
 	
 func _exit_tree() -> void:
-	simulation.stop_simulation()
-	
-	simulation = null
+	UniverseSimulation.stop_simulation()
 	
 func start_game(save_directory: String) -> void:
-	var instance: Node = instance_schematic.instantiate()
+	var game_instance = instance_schematic.instantiate()
 	
-	instance.simulation = simulation
+	game_instance.save_directory = save_directory
 	
-	add_child(instance)
-	
-	instance.load_galaxy(save_directory)
+	add_child(game_instance)
--- App/Game/game_instance.gd
+++ App/Game/game_instance.gd
@@ -1,34 +1,33 @@
 extends Node
 
-var simulation: UniverseSimulation
+var save_directory: String
 
 @export var mesh: Mesh
 
 func _enter_tree() -> void:
-	simulation.connect(&"connected_to_galaxy", connected_to_galaxy)
+	UniverseSimulation.connect(&"connected_to_galaxy", connected_to_galaxy)
+	
+	UniverseSimulation.start_local_galaxy(save_directory, "full_galaxy", UniverseSimulation.SERVER_TYPE_LOCAL)
+	
+	$Camera.make_current()
 	
 func _exit_tree() -> void:
-	simulation.disconnect_from_galaxy()
+	UniverseSimulation.disconnect_from_galaxy()
 	
-	simulation = null
+	UniverseSimulation.disconnect(&"connected_to_galaxy", connected_to_galaxy)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if (!simulation.progress(delta)):
+	if (!UniverseSimulation.progress(delta)):
 		printerr("world progress failed")
 	
 	$Camera.do_camera_controls(delta)
 	
 	var transform: Transform3D = $Camera.transform
 	
-	simulation.debug_command(&"set_debug_camera_transform", [transform])
+	UniverseSimulation.debug_command(&"set_debug_camera_transform", [transform])
 	
 	$Speed.text = "Speed: " + str($Camera.accelerator)
-
-func load_galaxy(galaxy_directory: String) -> void:
-	$Camera.make_current()
-	
-	simulation.start_local_galaxy(galaxy_directory, &"full_galaxy", UniverseSimulation.SERVER_TYPE_LOCAL)
 
 func connected_to_galaxy() -> void:
 	pass
