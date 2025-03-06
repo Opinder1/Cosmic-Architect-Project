8f896db0e31b7d2e544e998cad3ca236f99be064
Opinder <opinder.13@gmail.com>
2025-03-06 23:26:53 +0000

    Rename UniverseSimulation to UniverseServer

--- App/Game/game.gd
+++ App/Game/game.gd
@@ -3,12 +3,12 @@ extends Screen
 @onready var instance_schematic = preload("res://App/Game/game_instance.tscn")
 	
 func _enter_tree() -> void:
-	UniverseSimulation.set_render_context(get_viewport().find_world_3d().scenario)
+	UniverseServer.set_render_context(get_viewport().find_world_3d().scenario)
 	
-	UniverseSimulation.start_simulation(UniverseSimulation.THREAD_MODE_MULTI_THREADED)
+	UniverseServer.start_simulation(UniverseServer.THREAD_MODE_MULTI_THREADED)
 	
 func _exit_tree() -> void:
-	UniverseSimulation.stop_simulation()
+	UniverseServer.stop_simulation()
 	
 func start_game(save_directory: String) -> void:
 	var game_instance = instance_schematic.instantiate()
--- App/Game/game_instance.gd
+++ App/Game/game_instance.gd
@@ -5,27 +5,27 @@ var save_directory: String
 @export var mesh: Mesh
 
 func _enter_tree() -> void:
-	UniverseSimulation.connect(&"connected_to_galaxy", connected_to_galaxy)
+	UniverseServer.connect(&"connected_to_galaxy", connected_to_galaxy)
 	
-	UniverseSimulation.start_local_galaxy(save_directory, "full_galaxy", UniverseSimulation.SERVER_TYPE_LOCAL)
+	UniverseServer.start_local_galaxy(save_directory, "full_galaxy", UniverseServer.SERVER_TYPE_LOCAL)
 	
 	$Camera.make_current()
 	
 func _exit_tree() -> void:
-	UniverseSimulation.disconnect_from_galaxy()
+	UniverseServer.disconnect_from_galaxy()
 	
-	UniverseSimulation.disconnect(&"connected_to_galaxy", connected_to_galaxy)
+	UniverseServer.disconnect(&"connected_to_galaxy", connected_to_galaxy)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if (!UniverseSimulation.progress(delta)):
+	if (!UniverseServer.progress(delta)):
 		printerr("world progress failed")
 	
 	$Camera.do_camera_controls(delta)
 	
 	var transform: Transform3D = $Camera.transform
 	
-	UniverseSimulation.debug_command(&"set_debug_camera_transform", [transform])
+	UniverseServer.debug_command(&"set_debug_camera_transform", [transform])
 	
 	$Speed.text = "Speed: " + str($Camera.accelerator)
 
