d7ae26209962c7b6311f8e2db56b9269b8637408
Opinder <opinder.13@gmail.com>
2025-07-06 22:13:13 +0100

    Update to latest c++ extension version

--- App/Game/game_instance.gd
+++ App/Game/game_instance.gd
@@ -3,12 +3,12 @@ extends Node
 @export var mesh: Mesh
 
 func _enter_tree() -> void:
-	UniverseServer.connect(&"connected_to_galaxy", connected_to_galaxy)
+	UniverseServer.connect(&"connected_to_universe", connected_to_universe)
 	
 	$Camera.make_current()
 	
 func _exit_tree() -> void:
-	UniverseServer.disconnect(&"connected_to_galaxy", connected_to_galaxy)
+	UniverseServer.disconnect(&"connected_to_universe", connected_to_universe)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -20,5 +20,5 @@ func _process(delta: float) -> void:
 	
 	$Speed.text = "Speed: " + str($Camera.accelerator)
 
-func connected_to_galaxy() -> void:
+func connected_to_universe() -> void:
 	pass
--- App/app.gd
+++ App/app.gd
@@ -4,21 +4,27 @@ extends ScreenStack
 
 func _connect_signals() -> void:
 	$MainMenu.game_selected.connect(_on_game_selected)
+	UniverseServer.connect("update_debug_info", _on_update_debug_info)
 	
 func _on_game_selected(save_directory: String, is_multiplayer: bool) -> void:
 	set_screen($LoadOverlay)
 	
-	UniverseServer.start_local_galaxy(save_directory, "full_galaxy", UniverseServer.SERVER_TYPE_LOCAL, get_viewport().find_world_3d().scenario)
+	UniverseServer.start_local_universe(save_directory, "full_universe", UniverseServer.SERVER_TYPE_LOCAL, get_viewport().find_world_3d().scenario)
 	
 	$Game.add_child(instance_schematic.instantiate())
 
 	set_screen($Game)
+	overlay_screen($DebugOverlay)
+
+func _on_update_debug_info(debug_info: String) -> void:
+	$DebugOverlay/DebugStats.text = debug_info
 	
 func _ready() -> void:
 	_connect_signals()
 	
 	set_screen($MenuScene)
 	overlay_screen($MainMenu)
+	overlay_screen($DebugOverlay)
 	
 func _process(delta: float) -> void:
 	CommandServer.flush()
@@ -29,9 +35,11 @@ func _enter_tree() -> void:
 	UniverseServer.start_simulation(UniverseServer.THREAD_MODE_MULTI_THREADED)
 	
 func _exit_tree() -> void:
-	UniverseServer.disconnect_from_galaxy()
+	UniverseServer.disconnect_from_universe()
 	
 	UniverseServer.stop_simulation()
+	
+	UniverseServer.wait_until_stopped()
 	
 	# Final flush for any left over commands
 	while CommandServer.has_commands_left():
